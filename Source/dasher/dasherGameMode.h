a2f4e5726a45c119b0311ea3ddd146120f30d78a
Ryan Ash <scarletspark@gmail.com>
2023-10-06 20:34:59 +0200

    initial commit

---
+++ Source/dasher/dasherGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "dasherGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AdasherGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AdasherGameMode();
+};
+
+
+
